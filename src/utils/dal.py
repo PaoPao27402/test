10d1ea6d27ad2e853b1d60fa349fbff3c790c59b
paopao27402 <paorui27@hotmail.com>
2024-04-09 16:35:59 +0300

    upgrade pip

--- src/utils/dal.py
+++ src/utils/dal.py
@@ -1,54 +1,58 @@
-# import mysql.connector
+import mysql.connector
 
-# class DAL:
+class DAL:
 
-#     def __init__(self):
-#         self.connection = mysql.connector.connect(
-#             host="localhost",
-#             user="root",
-#             password="P@22word2710",
-#             database="travel_agency"
-#         )
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="P@22word2710",
+            database="travel_agency"
+        )
 
-#     def get_table(self, sql, params=None):
-#         with self.connection.cursor(dictionary=True) as cursor:
-#             cursor.execute(sql, params)
-#             table = cursor.fetchall()
-#         return table
+    def get_table(self, sql, params=None):
+        with self.connection.cursor(dictionary=True) as cursor:
+            cursor.execute(sql, params)
+            table = cursor.fetchall()
+        return table
         
-#     def get_table_v1(self, sql, params=None):
-#         cursor = self.connection.cursor()
-#         cursor.execute(sql, params)
-#         table = cursor.fetchall()
-#         cursor.close()
-#         return table
+    def get_table_v1(self, sql, params=None):
+        cursor = self.connection.cursor()
+        cursor.execute(sql, params)
+        table = cursor.fetchall()
+        cursor.close()
+        return table
     
-#     def get_scalar(self, sql, params=None):
-#         with self.connection.cursor() as cursor:
-#             cursor.execute(sql, params)
-#             scalar = cursor.fetchone()
-#         return scalar
+    # def get_scalar(self, sql, params=None):
+    #     with self.connection.cursor() as cursor:
+    #         cursor.execute(sql, params)
+    #         scalar = cursor.fetchone()
+    #     return scalar
         
-#     def insert(self, sql, params=None):
-#         with self.connection.cursor() as cursor:
-#             cursor.execute(sql, params)
-#             self.connection.commit() # Save to database now.
-#             last_row_id = cursor.lastrowid
-#         return last_row_id 
+    # def insert(self, sql, params=None):
+    #     with self.connection.cursor() as cursor:
+    #         cursor.execute(sql, params)
+    #         self.connection.commit() # Save to database now.
+    #         last_row_id = cursor.lastrowid
+    #     return last_row_id 
 
-#     def update(self, sql, params=None):
-#         with self.connection.cursor() as cursor:
-#             cursor.execute(sql, params)
-#             self.connection.commit() # Save to database now.
-#             row_count = cursor.rowcount
-#         return row_count
+    # def update(self, sql, params=None):
+    #     with self.connection.cursor() as cursor:
+    #         cursor.execute(sql, params)
+    #         self.connection.commit() # Save to database now.
+    #         row_count = cursor.rowcount
+    #     return row_count
        
-#     def delete(self, sql, params=None):
-#         with self.connection.cursor() as cursor:
-#             cursor.execute(sql, params)
-#             self.connection.commit() # Save to database now.
-#             row_count = cursor.rowcount
-#         return row_count
+    # def delete(self, sql, params=None):
+    #     with self.connection.cursor() as cursor:
+    #         cursor.execute(sql, params)
+    #         self.connection.commit() # Save to database now.
+    #         row_count = cursor.rowcount
+    #     return row_count
     
-#     def close(self):
-#         self.connection.close()
+    def close(self):
+        self.connection.close()
+
+d = DAL()
+sql = "SELECT * FROM travel_agency.vacations_tbl"
+d.close()
