494fd608179181637fceffa6ed8a74fabc1a2256
paopao27402 <paorui27@hotmail.com>
2024-04-11 14:53:14 +0300

    user logic last update

--- src/logic/user_logic.py
+++ src/logic/user_logic.py
@@ -9,14 +9,29 @@ class UserLogic:
         sql = "INSERT INTO travel_agency.users_tbl (first_name, last_name, email, password, role_ID) VALUES (%s, %s, %s, %s, %s)"
         params = (first_name, last_name, email, password, role_ID)
         result = self.dal.insert(sql, params)
-        results = UserModel.dictionaries_to_users(result)  # convert dict to object
+        results = UserModel.dictionaries_to_users(result) 
 
         return results
+    
+    def get_user_by_mail_id(self, email, password):
+        sql = "SELECT * FROM travel_agency.users_tbl WHERE email = %s AND password = %s"
+        params = (email, password)
+        result = self.dal.get_scalar(sql, params)
+        results = UserModel.dictionaries_to_users(result) 
+
+        return results
+    
+    def check_email_existence(self, email):
+        sql = "SELECT COUNT(*) FROM travel_agency.users_tbl WHERE email = %s"
+        params = (email,)
+        result = self.dal.get_scalar(sql, params)
+
+        return result['COUNT(*)'] > 0
 
     def get_all_users(self):
         sql = "SELECT * FROM travel_agency.users_tbl"
         result = self.dal.get_table(sql)
-        results = UserModel.dictionaries_to_users(result) # convert dict to object
+        results = UserModel.dictionaries_to_users(result)
         
         return results
     
