a20ff8185a6a5cda1352bff77f42b6b4735281ad
paopao27402 <paorui27@hotmail.com>
2024-04-12 22:26:30 +0300

    test user facade

--- src/travel_agancy_app.py
+++ src/travel_agancy_app.py
@@ -1,26 +1,67 @@
-from facades.vacations_facades import *
+# from facades.vacations_facades import *
 from facades.users_facades import *
-from facades.users_facades import *
+# from facades.likes_facades import *
 
-with VacationFacade() as facade:
+class Test:
+    def __init__(self):
+        # self.vacation_facade = VacationFacade()
+        self.users_facade = UsersFacade()
+        # self.likes_facade = LikesFacade()
 
-    random_vacations = facade.get_random_vacations()
-    print(len(random_vacations[0]))
+    def __enter__(self):
+        return self
 
-    facade.add_vacation()
-    print("A vacation has been added")
+    def __exit__(self, exc_type, exc_value, traceback):
+        # self.vacation_facade.close()
+        self.users_facade.close()
+        # self.likes_facade.close()
 
-# with UsersFacade() as facade:
+    # def test_vacation_facade(self):
+    #     print("Testing Vacation Facade...")
+    #     # Using with statement to ensure proper cleanup
+    #     with self.vacation_facade as facade:
+    #         # Test get_random_vacations
+    #         random_vacations = facade.get_random_vacations()
+    #         print("Random Vacations:", random_vacations)
 
-#     random_user = facade.get_random_user()
-#     print(len(random_user[0]))
+    #         # Test add_vacation (assuming parameters are provided)
+    #         result = facade.add_vacation(country_ID="Country", start_vacation_date="2024-05-01", end_vacation_date="2024-05-10", price=1000)
+    #         print("Add Vacation Result:", result)
 
-#     facade.add_vacation()
-#     print("A user has been added")
+    def test_users_facade(self):
+        print("Testing Users Facade...")
+    # Using with statement to ensure proper cleanup
+        with self.users_facade as facade:
+        # Test get_random_user
+            random_user = facade.get_random_user()
+            print("Random User Details:")
+            random_user.display()
 
-#     def register(self, email):
-#         facade.check_email_existence(email)
+            # result = facade.add_user(first_name="Juan", last_name="De la Cruz", email="juandela@example.com", password="p2545ssword", role_ID=2)
+            # print("Add User Result:", result)
 
-# with LikesFacade() as facade:
+            # result = facade.register(email="test@example.com", password="password")
+            # print("Register Result:", result)
+
+    # def test_likes_facade(self):
+    #     print("Testing Likes Facade...")
+    #     # Using with statement to ensure proper cleanup
+    #     with self.likes_facade as facade:
+    #         # Test add_vacation_like (assuming parameters are provided)
+    #         result = facade.add_vacation_like(user_ID=1, vacations_ID=1)
+    #         print("Add Like Result:", result)
+
+    #         # Test delete_vacation_like (assuming parameters are provided)
+    #         result = facade.delete_vacation_like(user_ID=1, vacations_ID=1)
+    #         print("Delete Like Result:", result)
+
+    def test_all(self):
+        # self.test_vacation_facade()
+        self.test_users_facade()
+        # self.test_likes_facade()
+
+with Test() as test_instance:
+    test_instance.test_all()
+
 
 
