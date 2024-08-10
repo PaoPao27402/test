598af92de11dc39d1919e8e060184d9800d85679
paopao27402 <paorui27@hotmail.com>
2024-08-10 20:20:56 +0300

    insert - dates requirements

--- src/facades/vacations_facades.py
+++ src/facades/vacations_facades.py
@@ -1,5 +1,6 @@
 from logic.vacation_logic import VacationLogic
 from models.vacation_model import *
+from utils.image_handler import *
 from flask import request
 
 class VacationFacade:
@@ -13,17 +14,16 @@ class VacationFacade:
     def get_one_vacation(self, vacations_ID):
         return self.logic.get_one_vacation(vacations_ID)
 
-    def add_vacation(self):
-        vacations_ID = request.form.get("vacations_ID")
+    def add_vacation(self, country_ID, vacation_description, start_vacation_date, end_vacation_date, price, vacation_pic_filename):
         country_ID = request.form.get("country_ID")
         vacation_description = request.form.get("vacation_description")
         start_vacation_date = request.form.get("start_vacation_date")
         end_vacation_date = request.form.get("end_vacation_date")
         price = request.form.get("price")
-        vacation_pic_filename = request.files["vacation_pic_filename"]
-        self.logic.add_vacation(vacations_ID, country_ID, vacation_description, start_vacation_date, end_vacation_date, price, vacation_pic_filename)
+        vacation_pic_filename = ImageHandler.save_image(vacation_pic_filename)
+        self.logic.add_vacation(country_ID, vacation_description, start_vacation_date, end_vacation_date, price, vacation_pic_filename)
     
-    def update_vacation(self):
+    def update_vacation(self, vacations_ID, country_ID, vacation_description, start_vacation_date, end_vacation_date, price, vacation_pic_filename):
         vacations_ID = request.form.get("vacations_ID")
         country_ID = request.form.get("country_ID")
         vacation_description = request.form.get("vacation_description")
--- src/views/vacations_view.py
+++ src/views/vacations_view.py
@@ -79,23 +79,28 @@ def get_image(image_name):
 def insert():
     try:
         auth_facade.block_anonymous()
-        if request.method == "GET": return render_template("insert.html", active="new")
+        if request.method == "GET":
+            return render_template("insert.html", active="new")
 
         country_ID = request.form.get('country')
         vacation_description = request.form.get('description')
         start_vacation_date = request.form.get('start_date')
         end_vacation_date = request.form.get('end_date')
         price = request.form.get('price')
-        vacation_pic_file = request.files.get('image') 
-        
+        vacation_pic_file = request.files.get('image')
+
+        if not start_vacation_date or not end_vacation_date:
+            return render_template("insert.html", error="Start date and end date are required.", active="new")
+
         facade.add_vacation(country_ID, vacation_description, start_vacation_date, end_vacation_date, price, vacation_pic_file)
-        
+
         return redirect(url_for("vacations_view.list"))
-    
+
     except AuthError as err:
         return redirect(url_for("auth_view.login"), error=err.message, credentials={})
     except ValidationError as err:
         return render_template("insert.html", error=err.message)
+
 
 
 
